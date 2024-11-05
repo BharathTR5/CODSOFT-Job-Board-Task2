6eaef5ffeae394a08818ffa55e8904cd06be9587
BharathTR <bharathtr70260@gmail.com>
2024-11-05 14:47:01 +0530

    Created Job-listing Section

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ if(bar){
     menu.classList.toggle("active")
   })
 }
+
+const sortBtns=document.querySelectorAll('.job-id > *');
+const sortItems=document.querySelectorAll('.jobs-container > *');
+
+sortBtns.forEach((btn) => {
+  btn.addEventListener('click', ()=>{
+    sortBtns.forEach((btn)=>{
+      btn.classList.remove('active')});
+      btn.classList.add('active');
+
+      const dataTarget = btn.getAttribute('data-target');
+
+      sortItems.forEach((item)=>{
+       item.classList.add('delete');
+       
+       if(dataTarget === item.getAttribute('data-item')){
+        item.classList.remove('delete');
+       }
+       if(dataTarget === 'all'){
+        item.classList.remove('delete');
+       }
+      })
+    })
+  });
